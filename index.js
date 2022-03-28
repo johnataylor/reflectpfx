1ef522fe1b236217c07e8447c9ccdd390d5cb535
John Taylor <johtaylo@microsoft.com>
2022-03-27 21:58:00 -0700

    make package and catch some more invalid input

---
+++ index.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+    lexer: require('./lib/lexer.js'),
+    parser: require('./lib/parser.js'),
+    utils: require('./lib/utils.js'),
+    structuredCondition: require('./lib/structuredCondition.js').structuredCondition  
+};
+
