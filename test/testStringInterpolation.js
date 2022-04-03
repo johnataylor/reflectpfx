695b1b704ac6e2a3ca42897135503c771beec6c7
John Taylor <johtaylo@microsoft.com>
2022-04-03 16:00:27 -0700

    integrate string interpolation in the lexer

--- test/testStringInterpolation.js
+++ test/testStringInterpolation.js
@@ -11,15 +11,17 @@ function getSegments(s) {
     function expression(s) {
         segments.push({ type: 'EXPRESSION', value: s });
     }
-    stringInterpolate.read(s, 0, literal, expression);
-    return segments;
+    var consumed = stringInterpolate.read(s, 0, literal, expression);
+    return { segments: segments, consumed: consumed };
 }
 
 // note the stringInterpolation.read function assumes the leading $" has been consumed
 
 describe('read string interpolation', function() {
     it('text1{expr1}text2{expr2}text3"', function(){
-        var segments = getSegments(this.test.title);
+        var result = getSegments(this.test.title);
+        var segments = result.segments;
+        assert.equal(this.test.title.length, result.consumed);
         assert.equal('LITERAL', segments[0].type);
         assert.equal('text1', segments[0].value);
         assert.equal('EXPRESSION', segments[1].type);
@@ -32,24 +34,32 @@ describe('read string interpolation', function() {
         assert.equal('text3', segments[4].value);
     });
     it('text1"', function(){
-        var segments = getSegments(this.test.title);
+        var result = getSegments(this.test.title);
+        var segments = result.segments;
+        assert.equal(this.test.title.length, result.consumed);
         assert.equal('LITERAL', segments[0].type);
         assert.equal('text1', segments[0].value);
     });
     it('{expr1}"', function(){
-        var segments = getSegments(this.test.title);
+        var result = getSegments(this.test.title);
+        var segments = result.segments;
+        assert.equal(this.test.title.length, result.consumed);
         assert.equal('EXPRESSION', segments[0].type);
         assert.equal('expr1', segments[0].value);
     });
     it('{expr1}{expr2}"', function(){
-        var segments = getSegments(this.test.title);
+        var result = getSegments(this.test.title);
+        var segments = result.segments;
+        assert.equal(this.test.title.length, result.consumed);
         assert.equal('EXPRESSION', segments[0].type);
         assert.equal('expr1', segments[0].value);
         assert.equal('EXPRESSION', segments[1].type);
         assert.equal('expr2', segments[1].value);
     });
     it('te""xt1{expr1}t""ex{{t2"""', function(){
-        var segments = getSegments(this.test.title);
+        var result = getSegments(this.test.title);
+        var segments = result.segments;
+        assert.equal(this.test.title.length, result.consumed);
         assert.equal('LITERAL', segments[0].type);
         assert.equal('te""xt1', segments[0].value);
         assert.equal('EXPRESSION', segments[1].type);
@@ -58,7 +68,9 @@ describe('read string interpolation', function() {
         assert.equal('t""ex{{t2""', segments[2].value);
     });
     it('text1{x + y * z}text2"', function(){
-        var segments = getSegments(this.test.title);
+        var result = getSegments(this.test.title);
+        var segments = result.segments;
+        assert.equal(this.test.title.length, result.consumed);
         assert.equal('LITERAL', segments[0].type);
         assert.equal('text1', segments[0].value);
         assert.equal('EXPRESSION', segments[1].type);
@@ -67,7 +79,9 @@ describe('read string interpolation', function() {
         assert.equal('text2', segments[2].value);
     });
     it('text1{With({a:4,b:6},a*b) + c}text2"', function(){
-        var segments = getSegments(this.test.title);
+        var result = getSegments(this.test.title);
+        var segments = result.segments;
+        assert.equal(this.test.title.length, result.consumed);
         assert.equal('LITERAL', segments[0].type);
         assert.equal('text1', segments[0].value);
         assert.equal('EXPRESSION', segments[1].type);
@@ -76,7 +90,9 @@ describe('read string interpolation', function() {
         assert.equal('text2', segments[2].value);
     });
     it(' text1 {expr1}  text2  {expr2}   text3   "', function(){
-        var segments = getSegments(this.test.title);
+        var result = getSegments(this.test.title);
+        var segments = result.segments;
+        assert.equal(this.test.title.length, result.consumed);
         assert.equal('LITERAL', segments[0].type);
         assert.equal(' text1 ', segments[0].value);
         assert.equal('EXPRESSION', segments[1].type);
@@ -89,7 +105,9 @@ describe('read string interpolation', function() {
         assert.equal('   text3   ', segments[4].value);
     });
     it(' text1 {expr1}  text2  {expr2}   text3   "', function(){
-        var segments = getSegments(this.test.title);
+        var result = getSegments(this.test.title);
+        var segments = result.segments;
+        assert.equal(this.test.title.length, result.consumed);
         assert.equal('LITERAL', segments[0].type);
         assert.equal(' text1 ', segments[0].value);
         assert.equal('EXPRESSION', segments[1].type);
@@ -102,14 +120,18 @@ describe('read string interpolation', function() {
         assert.equal('   text3   ', segments[4].value);
     });
     it(' t e x t { e x p r }"', function(){
-        var segments = getSegments(this.test.title);
+        var result = getSegments(this.test.title);
+        var segments = result.segments;
+        assert.equal(this.test.title.length, result.consumed);
         assert.equal('LITERAL', segments[0].type);
         assert.equal(' t e x t ', segments[0].value);
         assert.equal('EXPRESSION', segments[1].type);
         assert.equal(' e x p r ', segments[1].value);
     });
     it('text1{ {{{{{{{{{{hello}}}}}}}}}} }text2"', function(){
-        var segments = getSegments(this.test.title);
+        var result = getSegments(this.test.title);
+        var segments = result.segments;
+        assert.equal(this.test.title.length, result.consumed);
         assert.equal('LITERAL', segments[0].type);
         assert.equal('text1', segments[0].value);
         assert.equal('EXPRESSION', segments[1].type);
@@ -118,7 +140,10 @@ describe('read string interpolation', function() {
         assert.equal('text2', segments[2].value);
     });
     it('text1{expr1}text2{expr2}text3" & a & b & c', function(){
-        var segments = getSegments(this.test.title);
+        var result = getSegments(this.test.title);
+        var segments = result.segments;
+        var expectedLength = this.test.title.indexOf('"') + 1;
+        assert.equal(expectedLength, result.consumed);
         assert.equal('LITERAL', segments[0].type);
         assert.equal('text1', segments[0].value);
         assert.equal('EXPRESSION', segments[1].type);
@@ -129,5 +154,16 @@ describe('read string interpolation', function() {
         assert.equal('expr2', segments[3].value);
         assert.equal('LITERAL', segments[4].type);
         assert.equal('text3', segments[4].value);
+    });
+    it('text1{expr1 & $"expr1.text1{expr1.expr1}expr1.text2"}text2"', function(){
+        var result = getSegments(this.test.title);
+        var segments = result.segments;
+        assert.equal(this.test.title.length, result.consumed);
+        assert.equal('LITERAL', segments[0].type);
+        assert.equal('text1', segments[0].value);
+        assert.equal('EXPRESSION', segments[1].type);
+        assert.equal('expr1 & $"expr1.text1{expr1.expr1}expr1.text2"', segments[1].value);
+        assert.equal('LITERAL', segments[2].type);
+        assert.equal('text2', segments[2].value);
     });
 });
